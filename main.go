f3d2452d174e70f84f650ab9833079e017c13a34
dubs11kt <dubs11kt@gmail.com>
2022-06-29 22:23:34 +0900

    update: request loop

--- main.go
+++ main.go
@@ -6,15 +6,20 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func main() {
 	flag.Parse()
 	url := strings.Join(flag.Args(), " ")
 
-	resp, _ := http.Get(url)
-	defer resp.Body.Close()
+	for true {
+		resp, _ := http.Get(url)
+		defer resp.Body.Close()
 
-	byteArray, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(byteArray))
+		byteArray, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(byteArray))
+
+		time.Sleep(time.Second * 1)
+	}
 }
